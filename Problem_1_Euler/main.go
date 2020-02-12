9dc0b8fb4b47db7e0b21cd6739ac8a5b8904ede2
Bakare Emmanuel <bakare@deimos.co.za>
2020-02-12 02:46:54 +0100

    Added problem 1 and 2 of project euler problem

---
+++ Problem_1_Euler/main.go
@@ -0,0 +1,31 @@
+package main
+
+
+// https://projecteuler.net/problem=1
+
+import "fmt"
+
+
+func calcArithmeticSum(a int, n int) int {
+	if n == 0 {
+		return 0
+	}
+	ans := n * (a * (n+1)) / 2
+	fmt.Printf("Answer with a:%d and n:%d is %d\n", a, n , ans)
+	return ans
+}
+
+// O(1) space and time
+func MultiplesOf3And5(num int) int {
+	divBy3 := (num - 1) / 3
+	divBy5 := (num - 1) / 5
+	divBy15 := (num - 1) / 15
+	return calcArithmeticSum(3, divBy3) + calcArithmeticSum(5, divBy5) - calcArithmeticSum(15, divBy15)
+}
+
+
+func main() {
+	num := 1000
+	ans := MultiplesOf3And5(num)
+	fmt.Println(ans)
+}
---
+++ Problem_2_Euler/main.go
@@ -0,0 +1,39 @@
+package main
+
+// https://projecteuler.net/problem=2
+
+
+import (
+	"fmt"
+	"math"
+	)
+
+func BinetFormula(n float64) float64 {
+	sqrt5 := math.Sqrt(5)
+	phi := (1 + sqrt5) / 2
+	ans := math.Round(math.Pow(phi, n) / sqrt5)
+	fmt.Printf("Value at n: %f in fibonnaci sequence is: %f\n", n, ans)
+	return ans
+}
+
+// O(n) time and space
+// n - Number of even fibonacci occurrences
+func FibonnaciSum() int {
+	currValue := 0.0
+	n := 3.0
+	maxValue := 4* math.Pow(10, 6)
+	for BinetFormula(n) < maxValue {
+		currValue += BinetFormula(n)
+		fmt.Println(currValue)
+		// 1 2 3 5 8 13 21 34....
+		// Following the sequence, we see that the index for even numbers occurs at steps from 3 from 1
+		// 1 4 7 ....
+		// So no need to bother with the other sequences, just increment the index by 3 and move to the next even one
+		n += 3
+	}
+	return int(currValue)
+}
+
+func main(){
+	fmt.Println(FibonnaciSum())
+}
