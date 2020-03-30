7338dbf314800967d86a80b2ea5d2f4b33bfddec
Bakare Emmanuel <bakare@deimos.co.za>
2020-03-30 15:30:58 +0100

    Added solutions for problems 26

---
+++ Problem_26_Euler/main.go
@@ -0,0 +1,50 @@
+package main
+
+
+import (
+    "flag"
+    "fmt"
+)
+
+
+// https://projecteuler.net/problem=26
+
+func getRepetend(numer int, denom int) int {
+    pos := -1
+    base := 10
+    digitMap := map[int]int{}
+    for {
+        
+        if _, ok := digitMap[numer]; ok || numer == 0 {
+            break
+        }
+        digitMap[numer] = pos
+        z := base * numer / denom
+        numer = (numer * base) - (z * denom)
+        fmt.Println(numer, pos)
+        pos -= 1
+    }
+
+    return digitMap[numer] - pos
+}
+
+
+func main(){
+    window := flag.Int("window", 2, "Range to find the longest repetend in")
+    flag.Parse()
+   
+    fmt.Println(getRepetend(1, *window)) 
+    return
+    result, max := 0, 0
+    for *window > 1  {
+        *window --
+        temp := getRepetend(1, *window)
+        if temp > max {
+            result = * window
+            max = temp
+        } 
+        *window -- 
+    }
+    fmt.Println("Digit, Length: ",result, max) 
+}
+
