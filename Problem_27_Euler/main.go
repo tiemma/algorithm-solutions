c716b8826ed4e90f72fcb0d08f61058fc5cbdf6e
Bakare Emmanuel <bakare@deimos.co.za>
2020-03-30 23:59:14 +0100

    Added solution for 27

---
+++ Problem_27_Euler/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "math"
+)
+
+// https://projecteuler.net/problem=27
+
+func isPrime(n float64, a float64, b float64) bool{
+    val := math.Pow(n, 2) + (a * n) + b 
+    var i float64 = 2
+    if val <= 1 {
+        return false
+    }
+    for i <= math.Sqrt(val) {
+        if int(val) % int(i) == 0 {
+            return false
+        }
+        i += 1
+    }
+    return true
+}
+
+
+func main() {
+    vala := flag.Int("a", 1, "Range of values for a")
+    valb := flag.Int("b", 41, "Range of values for b")
+    flag.Parse()
+
+    a := - float64(*vala)
+    var max float64 = 1
+    var result float64 = 1
+    fmt.Println(result)
+    for a <= float64(*vala) {        
+        b := - float64(*valb)
+        for b <= float64(*valb) {
+            var n float64 = 0
+            for isPrime(n, a, b) {
+                n += 1
+            }
+            /*
+            if n > 1 {
+                fmt.Println(n - 2, a, b)
+            }*/
+            if n - 2 > max {
+                max = n - 2
+                result = a * b
+            }      
+            b += 1
+        }
+        a += 1
+    }
+    fmt.Println(result) 
+}
